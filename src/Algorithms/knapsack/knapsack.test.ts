3c3e13ad144417657bd4d67848b1faf1b5039f2e
Juha Tauriainen <juha@bin.fi>
2020-07-20 23:21:00 +0300

    Add tests and naive implementation for knapsack

--- src/Algorithms/knapsack/knapsack.test.ts
+++ src/Algorithms/knapsack/knapsack.test.ts
@@ -1,7 +1,39 @@
 import knapsack from "./knapsack";
 
 describe("knapsack algorithm", () => {
-  it("should pass", () => {
-    expect(knapsack()).toBe(true);
+  it("should return empty list when given weight is zero", () => {
+    expect(knapsack([], 0)).toEqual([]);
+  });
+
+  it("should return empty list when given list is empty", () => {
+    expect(knapsack([], 1000)).toEqual([]);
+  });
+
+  it("should return empty list when given list is empty and weight is zero", () => {
+    expect(knapsack([], 0)).toEqual([]);
+  });
+
+  it("should return the only item if only one item in the list and the item fits", () => {
+    expect(
+      knapsack([{ title: "test", seconds: 123, id: "test" }], 123)
+    ).toEqual([{ title: "test", seconds: 123, id: "test" }]);
+  });
+
+  it("should return empty list if all the videos are shorter than given length", () => {
+    expect(knapsack([{ title: "test", seconds: 123, id: "test" }], 1)).toEqual(
+      []
+    );
+  });
+
+  it("should return a list which only contains elements which combined, fit into the given weight", () => {
+    expect(
+      knapsack(
+        [
+          { title: "test", seconds: 123, id: "test" },
+          { title: "test2", seconds: 123, id: "test2" },
+        ],
+        123
+      )
+    ).toEqual([{ title: "test", seconds: 123, id: "test" }]);
   });
 });
--- src/Algorithms/knapsack/knapsack.ts
+++ src/Algorithms/knapsack/knapsack.ts
@@ -1,3 +1,19 @@
-const knapsack = (): boolean => true;
+import { YoutubeItem } from "../../types";
+
+const knapsack = (list: YoutubeItem[], weight: number): YoutubeItem[] => {
+  const result = [];
+  let accumulatedWeight = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let index = 0; index < list.length; index++) {
+    const element = list[index];
+
+    if (element.seconds <= weight && accumulatedWeight + weight <= weight) {
+      accumulatedWeight += element.seconds;
+      result.push(element);
+    }
+  }
+
+  return result;
+};
 
 export default knapsack;
