385149827843b74b43a0269a76c8726b33badfe4
Juha Tauriainen <juha@bin.fi>
2020-07-22 18:49:14 +0300

    Add random function

--- src/Utils/math.ts
+++ src/Utils/math.ts
@@ -2,6 +2,7 @@ import { YoutubeItem } from "../types";
 
 const addSeconds = (a: number, { seconds }: YoutubeItem): number => a + seconds;
 
-const sumSeconds = (list: YoutubeItem[]): number => list.reduce(addSeconds, 0);
+export const sumSeconds = (list: YoutubeItem[]): number =>
+  list.reduce(addSeconds, 0);
 
-export default sumSeconds;
+export const random = (): number => Math.random();
