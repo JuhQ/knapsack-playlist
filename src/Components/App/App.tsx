83e8768ebefd8273e8ef6d42dfb3cce0f3421342
Juha Tauriainen <juha@bin.fi>
2020-07-22 20:19:21 +0300

    Update app component styles

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,25 +1,33 @@
 import "./App.css";
 
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 import knapsack from "../../Algorithms/knapsack/knapsack";
 import YoutubeMusic from "../../Models/Youtube/Youtube";
 import sample from "../../Utils/utils";
 import EntertainmentSystem from "../EntertainmentSystem/EntertainmentSystem";
 
-const App = (): JSX.Element => (
-  <>
-    <h1 data-text="Playlist Generator">Playlist Generator</h1>
-    <span className="App-logo" role="img" aria-label="rock on!">
-      🎸
-    </span>
-    <span className="App-logo" role="img" aria-label="rock on!">
-      🤘
-    </span>
+const playlistLength = 3600;
 
-    <EntertainmentSystem list={knapsack(sample(YoutubeMusic(), 20), 3600)} />
+const App = (): JSX.Element => (
+  <Container fluid>
+    <Container textAlign="center">
+      <h1 data-text="Playlist Generator">Playlist Generator</h1>
+      <span className="App-logo" role="img" aria-label="rock on!">
+        🎸
+      </span>
+      <span className="App-logo" role="img" aria-label="rock on!">
+        🤘
+      </span>
+    </Container>
+
+    <EntertainmentSystem
+      list={knapsack(sample(YoutubeMusic(), 20), playlistLength)}
+      length={playlistLength}
+    />
     <span className="security">Knapsack generated playlists rule!</span>
-  </>
+  </Container>
 );
 
 export default App;
