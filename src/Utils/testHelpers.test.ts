a8f3ba6aaacf54b2d2abf588764571433d07fed5
Juha Tauriainen <juha@bin.fi>
2020-07-27 01:18:02 +0300

    Restructure testhelper to generate queue

--- src/Utils/testHelpers.test.ts
+++ src/Utils/testHelpers.test.ts
@@ -2,6 +2,6 @@ import generateTestData from "./testHelpers";
 
 describe("Test helpers", () => {
   it("should generate test data", () => {
-    expect(generateTestData(20, 20)).toMatchSnapshot();
+    expect(generateTestData(20, 20).all()).toMatchSnapshot();
   });
 });
--- src/Utils/testHelpers.ts
+++ src/Utils/testHelpers.ts
@@ -1,11 +1,17 @@
-import { YoutubeItem } from "../types";
+import Queue from "../Datastructures/queue/queue";
 
-// This helper will generate n-length array of test data
-const generateTestData = (n: number, seconds = 1): YoutubeItem[] =>
-  [...Array(n).fill(null)].map((_, i) => ({
-    id: `${i}`,
-    title: "test",
-    seconds,
-  }));
+// This helper will generate n-length queue of test data
+const generateTestData = (n: number, seconds = 1): Queue => {
+  const queue = new Queue();
+
+  for (let i = 0; i < n; i++) {
+    queue.enqueue({
+      id: `${i}`,
+      title: "test",
+      seconds,
+    });
+  }
+  return queue;
+};
 
 export default generateTestData;
