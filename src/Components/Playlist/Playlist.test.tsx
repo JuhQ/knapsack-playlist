8eddaeda268cc747f933677fb11256c69d79e552
Juha Tauriainen <juha@bin.fi>
2020-07-27 02:25:42 +0300

    Use queue in playlist

--- src/Components/Playlist/Playlist.test.tsx
+++ src/Components/Playlist/Playlist.test.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
+import Queue from "../../Datastructures/queue/queue";
 import Playlist from "./Playlist";
 
 describe("Playlist", () => {
   it("should render list of songs", () => {
+    const list = new Queue();
+    list.merge([
+      { id: "1", title: "test", seconds: 60 },
+      { id: "2", title: "test 2", seconds: 60 },
+    ]);
+
     const component = renderer.create(
-      <Playlist
-        list={[
-          { id: "1", title: "test", seconds: 60 },
-          { id: "2", title: "test 2", seconds: 60 },
-        ]}
-        onChange={() => null}
-      />
+      <Playlist list={list} onChange={() => null} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
--- src/Components/Playlist/Playlist.tsx
+++ src/Components/Playlist/Playlist.tsx
@@ -3,18 +3,19 @@ import "./Playlist.css";
 import React from "react";
 import { Item } from "semantic-ui-react";
 
+import Queue from "../../Datastructures/queue/queue";
 import { YoutubeItem } from "../../types";
 import PlaylistItem from "./PlaylistItem/PlaylistItem";
 
 interface Props {
-  list: YoutubeItem[];
+  list: Queue;
   onChange: (item: YoutubeItem) => void;
 }
 
 const Playlist: React.FC<Props> = ({ list, onChange }: Props) => (
   <div className="playlist">
     <Item.Group>
-      {list.map((item) => (
+      {list.all().map((item) => (
         <PlaylistItem item={item} onClick={onChange} key={item.id} />
       ))}
     </Item.Group>
