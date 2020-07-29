d6e6db87e12b3cbb4ff69cdec57a483421055e08
Juha Tauriainen <juha@bin.fi>
2020-07-29 22:39:07 +0300

    Restructure utils to use array list

--- src/Utils/utils.test.tsx
+++ src/Utils/utils.test.tsx
@@ -1,18 +1,19 @@
+import ArrayList from "../Datastructures/ArrayList/ArrayList";
+import { YoutubeItem } from "../types";
 import sample from "./utils";
 
 describe("utils", () => {
   it("should sample list", () => {
-    const list = [
-      { title: "test", seconds: 100, id: "test" },
-      { title: "test1", seconds: 100, id: "test1" },
-      { title: "test2", seconds: 100, id: "test2" },
-      { title: "test3", seconds: 100, id: "test3" },
-      { title: "test4", seconds: 100, id: "test4" },
-      { title: "test5", seconds: 100, id: "test5" },
-      { title: "test6", seconds: 100, id: "test6" },
-    ];
+    const list = new ArrayList<YoutubeItem>(7);
+    list.push({ title: "test", seconds: 100, id: "test" });
+    list.push({ title: "test1", seconds: 100, id: "test1" });
+    list.push({ title: "test2", seconds: 100, id: "test2" });
+    list.push({ title: "test3", seconds: 100, id: "test3" });
+    list.push({ title: "test4", seconds: 100, id: "test4" });
+    list.push({ title: "test5", seconds: 100, id: "test5" });
+    list.push({ title: "test6", seconds: 100, id: "test6" });
 
     expect(sample(list, 3)).not.toEqual(list);
-    expect(sample(list, 3).length).toEqual(3);
+    expect(sample(list, 3).size()).toEqual(3);
   });
 });
--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,8 +1,12 @@
+import ArrayList from "../Datastructures/ArrayList/ArrayList";
 import { YoutubeItem } from "../types";
 import { random } from "./math";
 
-const sample = (list: YoutubeItem[], size: number): YoutubeItem[] =>
-  [...list]
+const sample = (
+  list: ArrayList<YoutubeItem>,
+  size: number
+): ArrayList<YoutubeItem> =>
+  list
     .map((item) => ({ ...item, sort: random() }))
     .sort((a, b) => a.sort - b.sort)
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
