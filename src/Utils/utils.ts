81c097ee55be44a24122b22cd8141ff919e9bd6e
Juha Tauriainen <juha@bin.fi>
2020-07-31 22:40:04 +0300

    Use quicksort in sample function

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,14 +1,20 @@
+import quicksort from "../Algorithms/quicksort/quicksort";
 import ArrayList from "../Datastructures/ArrayList/ArrayList";
 import { YoutubeItem } from "../types";
 import { random } from "./math";
 
+/**
+ * This function returns a given length randomized sample of given list.
+ * Randomization happens by extending the YouTubeItem data type with sort key, which is given an random value
+ * The list is then sorted based on this random value and the last n-values are returned
+ * @param list ArrayList<YoutubeItem>
+ * @param size number
+ */
 const sample = (
   list: ArrayList<YoutubeItem>,
   size: number
 ): ArrayList<YoutubeItem> =>
-  list
-    .map((item) => ({ ...item, sort: random() }))
-    .sort((a, b) => a.sort - b.sort)
+  quicksort(list.map((item) => ({ ...item, sort: random() })))
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     .map(({ sort, ...item }) => ({ ...item }))
     .slice(-size);
